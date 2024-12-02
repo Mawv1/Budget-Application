932ff3dad1365f344616e5f7fced37c638c66cb1
Mawv1 <mateuszwasiak81@interia.pl>
2024-12-02 21:48:13 +0100

    Repaired slider

--- index.php
+++ index.php
@@ -77,7 +77,7 @@
                 </div>
         
                 <!-- Slider -->
-                <div class="slider-container">
+                <!-- <div class="slider-container">
                     <div class="slider">
                         <div class="slider-item">
                             <img src="pictures/slider-image1.jpg" alt="Item 1">
@@ -96,7 +96,61 @@
                         <button class="prev">&lt;</button>
                         <button class="next">&gt;</button>
                     </div>
-                </div>                
+                </div>                 -->
+                    <!-- Początek slidera -->
+                <div class="slider">
+                    <div class="slides">
+                        <!-- Początek przycisków nawigacyjnych -->
+                        <input type="radio" name="radio-btn" id="radio1">
+                        <input type="radio" name="radio-btn" id="radio2">
+                        <input type="radio" name="radio-btn" id="radio3">
+                        <!-- Koniec przycisków nawigacyjnych -->
+
+                        <!-- Początek zdjęć z zawartością -->
+                        <div class="slide first">
+                            <img src="pictures/hero.png" alt="Budżet domowy" class="slide-image">
+                            <div class="slide-content">
+                                <h1>Zarządzaj swoim budżetem z łatwością</h1>
+                                <p>Kontrola wydatków nigdy nie była prostsza.</p>
+                                <a href="login_module/register.php" class="cta-button">Rozpocznij za darmo</a>
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <img src="pictures/savings.png" alt="Planowanie oszczędności" class="slide-image">
+                            <div class="slide-content">
+                                <h1>Planuj oszczędności z nami</h1>
+                                <p>Twój plan finansowy na wyciągnięcie ręki.</p>
+                                <a href="login_module/register.php" class="cta-button">Rozpocznij za darmo</a>
+                            </div>
+                        </div>
+                        <div class="slide">
+                            <img src="pictures/dream_chasing.png" alt="Oszczędzanie na przyszłość" class="slide-image">
+                            <div class="slide-content">
+                                <h1>Bezpieczna przyszłość finansowa</h1>
+                                <p>Oszczędzaj na swoje marzenia.</p>
+                                <a href="login_module/register.php" class="cta-button">Rozpocznij za darmo</a>
+                            </div>
+                        </div>
+                        <!-- Koniec zdjęć z zawartością -->
+
+                        <!-- Początek automatyznej nawigacji -->
+                        <div class="navigation-auto">
+                            <div class="auto-btn1"></div>
+                            <div class="auto-btn2"></div>
+                            <div class="auto-btn3"></div>
+                        </div>
+                        <!-- Koniec automatyznej nawigacji -->
+                    </div>
+
+                    <!-- Początek ręcznej nawigacji -->
+                    <div class="navigation-manual">
+                        <label for="radio1" class="manual-btn"></label>
+                        <label for="radio2" class="manual-btn"></label>
+                        <label for="radio3" class="manual-btn"></label>
+                    </div>
+                    <!-- Koniec ręcznej nawigacji -->
+                </div>
+                <!-- Koniec slidera -->
             </div>
         </main>
     </div>
